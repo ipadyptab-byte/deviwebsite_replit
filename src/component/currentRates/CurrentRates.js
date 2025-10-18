34879bd0211eef6c37601ff6456cca84160d04ac
ipadyptab-byte <ipadyptab@gmail.com>
2025-10-18 22:29:32 +0530

    Update CurrentRates.js

--- src/component/currentRates/CurrentRates.js
+++ src/component/currentRates/CurrentRates.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BsGraphUpArrow } from 'react-icons/bs';
 import { ratesAPI } from '../../api/client';
 import borderLine from '../../images/border_line.png';
-import './CurrentRates.css';  
+import './CurrentRates.css';
 
 const CurrentRates = () => {
   const [rates, setRates] = useState({
@@ -13,46 +13,48 @@ const CurrentRates = () => {
   });
 
   useEffect(() => {
-  const fetchRates = async () => {
-    try {
-      const data = await ratesAPI.getRates();
-      if (data) {
-        setRates({
-          vedhani: data.vedhani,
-          ornaments22K: data.ornaments22k,
-          ornaments18K: data.ornaments18k,
-          silver: data.silver,
-        });
-      } else {
-        console.log("No rates found!");
+    const fetchRates = async () => {
+      try {
+        const data = await ratesAPI.getRates();
+        if (data) {
+          setRates({
+            vedhani: data.vedhani,
+            ornaments22K: data.ornaments22K,  // fixed case
+            ornaments18K: data.ornaments18K,  // fixed case
+            silver: data.silver,
+          });
+        } else {
+          console.log("No rates found!");
+        }
+      } catch (error) {
+        console.error("Error fetching rates:", error);
       }
-    } catch (error) {
-      console.error("Error fetching rates:", error);
-    }
-  };
-
-  fetchRates();
-}, []);
-
-
+    };
+    fetchRates();
+  }, []);
 
   return (
-    <div className="icon_container">
-      <span className="icon_wrapper">
-        <BsGraphUpArrow />
-      </span>  
-      <span className="title">Current Rates</span> 
-
+    <div className="icon-container">
+      <span className="icon-wrapper"><BsGraphUpArrow /></span>
+      <span className="title">Current Rates</span>
       <div className="tooltip">
         <h1>Today's Gold Rates</h1>
-        <div className='border-line'>
-        <img src={borderLine} alt='border line'/>
+        <div className="border-line">
+          <img src={borderLine} alt="border line" />
         </div>
         <ul>
-          <li className='rates'>Vedhani  <span>₹{rates.vedhani || "N/A"}</span></li><br/>
-          <li className='rates'> 22KT <span>₹{rates.ornaments22K || "N/A"}</span></li><br/>
-          <li className='rates'>18KT <span>₹{rates.ornaments18K || "N/A"}</span></li><br/>
-          <li className='rates'>Silver <span>₹{rates.silver || "N/A"}</span></li>
+          <li className="ratesVedhani">
+            <span>Vedhani:</span> <span>{rates.vedhani || "N/A"}</span>
+          </li>
+          <li className="rates22KT">
+            <span>22KT Ornaments:</span> <span>{rates.ornaments22K || "N/A"}</span>
+          </li>
+          <li className="rates18KT">
+            <span>18KT Ornaments:</span> <span>{rates.ornaments18K || "N/A"}</span>
+          </li>
+          <li className="ratesSilver">
+            <span>Silver:</span> <span>{rates.silver || "N/A"}</span>
+          </li>
         </ul>
       </div>
     </div>
